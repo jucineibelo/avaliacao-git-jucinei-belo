6597bb0beb4fa60eb46cb93aaeae484958246111
JUCINEI LUIZ BELO <jucinei.belo@alunos.sc.senac.br>
2025-07-07 19:52:03 -0300

    implementado calculadora

--- app/Main.java
+++ app/Main.java
@@ -1,5 +1,16 @@
+package app;
+
+import service.Calculadora;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Avaliação Git com Java");
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.setNumero1(5.3);
+        calculadora.setNumero2(10.1);
+        System.out.println("multiplicação: " + calculadora.multiplicacao());
+        System.out.println("soma: " + calculadora.soma());
+
     }
 }
--- service/Calculadora.java
+++ service/Calculadora.java
@@ -1,4 +1,32 @@
 package service;
 
 public class Calculadora {
+
+    private Double numero1;
+
+    private Double numero2;
+
+    public Double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(Double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public Double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(Double numero2) {
+        this.numero2 = numero2;
+    }
+
+    public Double soma() {
+        return numero1 + numero2;
+    }
+
+    public Double multiplicacao() {
+        return numero1 * numero2;
+    }
 }
